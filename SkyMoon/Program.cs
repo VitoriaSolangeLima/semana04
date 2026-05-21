1c3b8c7f89e5ce78af8f6e0b645190cb55ea65e9
Vitória Solange <152615603+VitoriaSolangeLima@users.noreply.github.com>
2026-05-21 15:01:20 +0000

    Aula do dia 21

---
+++ SkyMoon/Program.cs
@@ -0,0 +1,66 @@
+# Código do arquivo Program.cs
+using skymoon.Models;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.WebHost.UseUrls("http://0.0.0.0:8000");
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+Funcionario[] funcionarios = new Funcionario[100];
+int totalFuncionarios = 0;
+
+app.MapGet("/", () =>
+{
+    return Results.Ok("API SkyMoon funcionando com sucesso!");
+});
+
+/* app.MapPost("/funcionario", (JsonElement body) =>
+{
+    
+});
+
+app.MapGet("/funcionario", () =>
+{
+    
+});
+
+app.MapPatch("/funcionario/{id}", (int id, JsonElement body) =>
+{
+    
+});
+
+app.MapPut("/funcionario/{id}", (int id, JsonElement body) =>
+{   
+    
+});
+
+app.MapDelete("/funcionario", (int id) =>
+{
+    
+});
+
+app.MapGet("/funcionario/departamento/busca", (string departamento) =>
+{
+    
+});
+
+app.MapGet("/funcionario/busca", (string nome) =>
+{
+   
+}); */
+
+app.Run();
